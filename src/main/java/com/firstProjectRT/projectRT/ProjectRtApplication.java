72550ba6a5a739e5f0fdc9189a7d9904b27b23f1
RaulTeles <telesraul@gmail.com>
2024-12-19 15:34:49 -0300

    initial project

---
+++ src/main/java/com/firstProjectRT/projectRT/ProjectRtApplication.java
@@ -0,0 +1,13 @@
+package com.firstProjectRT.projectRT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firstProjectRT/projectRT/ProjectRtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstProjectRT.projectRT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
