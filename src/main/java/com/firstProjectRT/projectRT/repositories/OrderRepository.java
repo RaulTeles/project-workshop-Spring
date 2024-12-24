78964feba18e8a621c94cb2c245318ff3d706dc6
RaulTeles <telesraul@gmail.com>
2024-12-24 10:45:57 -0300

    create class order resource and repository

---
+++ src/main/java/com/firstProjectRT/projectRT/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.firstProjectRT.projectRT.repositories;
+
+import com.firstProjectRT.projectRT.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
